9966b7ef7ebb0d211dabf3404cd8da9f726f59d3
CUITMIR <cuitmir@163.com>
2026-05-26 10:35:01 +0800

    __init__.py

--- openstl/models/__init__.py
+++ openstl/models/__init__.py
@@ -8,7 +8,7 @@ from .phydnet_model import PhyDNet_Model
 from .predrnn_model import PredRNN_Model
 from .predrnnpp_model import PredRNNpp_Model
 from .predrnnv2_model import PredRNNv2_Model
-from .simvp_model import SimVP_Model
+from .RLNet_model import SimVP_Model
 from .mmvp_model import MMVP_Model
 from .swinlstm_model import SwinLSTM_D_Model, SwinLSTM_B_Model
 
