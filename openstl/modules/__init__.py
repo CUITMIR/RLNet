9a4116355dae13fbb7fa7e90110896d5589ee5c6
CUITMIR <cuitmir@163.com>
2026-05-26 10:41:08 +0800

    Update __init__.py

--- openstl/modules/__init__.py
+++ openstl/modules/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) CAIRI AI Lab. All rights reserved
-from .simvp_modules import (BasicConv2d, ConvSC, GroupConv2d, gInception_ST,)
+from .RLNet_modules import (BasicConv2d, ConvSC, GroupConv2d, gInception_ST,)
 
 
 __all__ = [ 'BasicConv2d', 'ConvSC', 'GroupConv2d', 'gInception_ST']
